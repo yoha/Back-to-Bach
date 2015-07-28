30537f8e524d90e1e888ef65a29902457cddbafc
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-28 11:58:34 +0700

    Added: logic for playcontrol and volume
    to add: app icon, launch screen wallpaper, song's album image, display song's title

--- Back to Bach/ViewController.swift
+++ Back to Bach/ViewController.swift
@@ -7,31 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.stopButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Stored properties
+    
+    var songPlayer: AVAudioPlayer!
+    let songFilePath: String! = NSBundle.mainBundle().pathForResource("Porno Graffitti - Lack", ofType: "mp3")
+    var playOrPause: (String, String) = ("Play", "Pause")
+    enum SongPotentialError: ErrorType {
+        case SongFileNotFound
+    }
 
     // MARK: - IBOutlet properties
     
     @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var songVolumeSlider: UISlider!
-    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     // MARK: - IBAction properties
     
     @IBAction func playPauseButtonPressed(sender: UIButton) {
+        do {
+            try self.songPlayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: self.songFilePath))
+            print(self.songFilePath)
+            self.songPlayer.prepareToPlay()
+            self.songPlayer.play()
+            self.playPauseButton.setTitle(self.isPlaying(), forState: UIControlState.Normal)
+            self.stopButton.enabled = true
+        }
+        catch SongPotentialError.SongFileNotFound {
+            print("Song file is not found...")
+        }
+        catch {
+            print("Unregistered error has occured...")
+        }
     }
     @IBAction func stopButtonPressed(sender: UIButton) {
+        self.songPlayer.stop()
+        self.playPauseButton.setTitle("Play", forState: UIControlState.Normal)
+        self.stopButton.enabled = false
     }
     
+    @IBAction func songVolumeSliderChanged(sender: UISlider) {
+        self.songPlayer.volume = sender.value
+    }
     
+    // MARK: - Local methods
+    
+    func isPlaying() -> String {
+        return self.playPauseButton.titleForState(UIControlState.Normal) == "Play" ? "Pause" : "Play"
+    }
 }
 
